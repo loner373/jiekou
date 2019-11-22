06f9177ccec580ab54f1b19fc0178c0de45b61f6
loner373 <1908558922@qq.com>
2019-11-22 17:05:29 +0800

    级联方式封装

--- wangt/configure/configure.py
+++ wangt/configure/configure.py
@@ -5,17 +5,39 @@ import requests
 from wangt.configure.cookices import Wangt
 
 
-class Configure(object):
-
+class Basics(object):
+    logging.basicConfig(level=logging.INFO)
     url = "http://t0st.ytzq.com:8443/web/bus/json?"
     cookies = Wangt.get_cookices()
+    method = "POST"
     params = {}
+    def set_params(self, params):
+        self.params = params
+        return self
 
     def run(self):
-        self.response = requests.post(self.url, cookies=self.cookies, params=self.params)
+        self.response = requests.request(
+            self.method,
+            self.url,
+            cookies=self.cookies,
+            params=self.params,
+        )
+        print(self.response)
         return self
 
     def validate(self, key, expect_value):
-        actual_value = getattr(self.response, key)
-        assert actual_value == expect_value
+        value = self.response.json()
+        for _key in key.split("."):
+            print("=======", key.split("."))
+            if isinstance(value, requests.Response):
+                value = getattr(value, _key)
+                print("+++++++++", value)
+            elif isinstance(value, dict):
+                value = value[_key]
+
+
+
+        # actual_value = getattr(self.response, key)
+        # assert value == expect_value
+
         return self
--- wangt/test_case/test_Protocol.py
+++ wangt/test_case/test_Protocol.py
@@ -1,51 +1,24 @@
 import json
 import logging
-
-import requests
-
 from wangt.Business.Jybl import Jyblyw
-from wangt.configure.configure import Configure
-from wangt.configure.cookices import Wangt
+from wangt.configure.configure import Basics
 
 
-class TestProtocol(object):
-    url = "http://t0st.ytzq.com:8443/web/bus/json?"
-    logging.basicConfig(level=logging.INFO)
-    cookies = Wangt.get_cookices()
-    params = {}
 
-    def set_params(self, params):
-        self.params = params
-        return self
-
-    def run(self):
-        self.response = requests.post(
-            self.url,
-            cookies=self.cookies,
-            params=self.params
-        )
-        return self
-
-    def validate(self, key, expect_value):
-        actual_value = getattr(self.response, key)
-        assert actual_value == expect_value
-        return self
-
+class TestProtocol(Basics):
     def test_Beiyonglxr(self):
         params = {
             "funcNo": "100966",
             "trade_no": "1100004603",
         }
-
-        # r = Jyblyw.Beiyong(self, **kwargs)
+        r = Jyblyw.Beiyong(self, **params)
         # logging.info(json.dumps(r.json(), indent=2))
-        # assert r.status_code == 200
+        print("ffdjf++++++", r.json())
         # assert r.json()["results"][0]["linkman_tel"] == "18926080076"
-
         TestProtocol \
             .set_params(self, params) \
             .run() \
-            .validate("status_code", 200)
+            .validate("results.0.linkman_mobile_tel", "18926080076")
 
     def test_Chaxunsfz(self):
         kwargs = {
@@ -55,10 +28,10 @@ class TestProtocol(object):
         # r = Jyblyw.Beiyong(self, **kwargs)
         # logging.info(json.dumps(r.json(), indent=2))
         # assert r.json()["results"][0]["mobile_tel"] == "18926080076"
-        TestProtocol\
-            .set_params(self, kwargs)\
-            .run()\
-            .validate("status_code", 200)
+        TestProtocol \
+            .set_params(self, kwargs) \
+            .run() \
+            .validate("results.mobile_tel", "18926080076")
 
     # @pytest.mark.parametrize("ziduan","velus", [
     #         ("exp_date", "true"),
@@ -73,13 +46,17 @@ class TestProtocol(object):
             "crm_client_type": "0",
             "funcNo": "160102",
         }
-        r = Jyblyw.Beiyong(self, **kwargs)
-        logging.info(json.dumps(r.json(), indent=2))
-        assert r.json()["results"][0]["exp_date"] == "20191114"
-        assert r.json()["results"][0]["isOutTime"] == "true"
-        assert r.json()["results"][0]["rating_date"] == "20191113"
-        assert r.json()["results"][0]["user_role"] == "1"
-        assert r.json()["results"][0]["next_rating_date"] == "20191113"
+        # r = Jyblyw.Beiyong(self, **kwargs)
+        # logging.info(json.dumps(r.json(), indent=2))
+        # assert r.json()["results"][0]["exp_date"] == "20191114"
+        # assert r.json()["results"][0]["isOutTime"] == "true"
+        # assert r.json()["results"][0]["rating_date"] == "20191113"
+        # assert r.json()["results"][0]["user_role"] == "1"
+        # assert r.json()["results"][0]["next_rating_date"] == "20191113"
+        TestProtocol \
+            .set_params(self, kwargs) \
+            .run() \
+            .validate("status_code", 200)
 
     def test_Kechuangban(self):
         kwargs = {
